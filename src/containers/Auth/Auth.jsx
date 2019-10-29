51650f133343bb998b01ded485c63002e8ef6215
swarog0777 <ruslan1guchetl@gmail.com>
2019-10-29 17:47:05 +0300

    smallfix2

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import './Auth.scss';
+import Button from "../../components/UI/Button/Button";
 
 class Auth extends React.Component {
+
+  loginHandler = () => {
+
+  };
+
+  registerHandler = () => {
+
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className="auth">
+        <div>
+          <h1>Auth</h1>
+
+          <form className="auth-form"
+                onSubmit={this.submitHandler}
+          >
+            <input type="text"/>
+            <input type="text"/>
+
+            <Button
+              type={'success'}
+              onClick={this.loginHandler}
+            >Войти</Button>
+            <Button
+              type={'primary'}
+              onClick={this.registerHandler}
+            >Зарегистрироваться</Button>
+          </form>
+        </div>
       </div>
     );
   }
